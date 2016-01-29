0da58100a216c3826291802aa3d3f7b0893030f7
Andrew Cho <cho-andrew@hotmail.com>
2016-01-29 07:59:31 -0800

    add functionality to determine the cost of pizza based on properities previously defined.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-function Pizza (sizes,toppings,quantity) {
-  this.sizes = sizes;
+function Pizza (size,toppings,quantity) {
+  this.size = size;
   this.toppings = toppings;
   this.quantity = quantity;
+}
+
+Pizza.prototype.price = function() {
+  if (this.size === "Small") {
+    return 8 * this.quantity;
+  } else if (this.size === "Medium") {
+      return 10 * this.quantity;
+  } else if (this.size === "Large") {
+      return 12 * this.quantity;
+  } else {
+      return this.size * this.quantity;
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,15 @@
-describe("Pizza",function(){
+describe("Pizza", function(){
   it("will create a pizza with three seperate properities which user will build",function() {
     var newPizza = new Pizza ("Small", ["Pepperoni", "Sausage", "Mushrooms"], 3);
-    expect(newPizza.sizes).to.equal("Small");
+    expect(newPizza.size).to.equal("Small");
     expect(newPizza.toppings).to.eql(["Pepperoni", "Sausage", "Mushrooms"]);
-    expect(newPizza.quantity).to.equal(2);
+    expect(newPizza.quantity).to.equal(3);
+  });
+});
+
+describe("Pizza.Prototype.Price", function(){
+  it("will calculate pizza's cost", function(){
+    var newPizza = new Pizza ("Medium", ["Ham"], 2);
+    expect(newPizza.price()).to.equal(20)
   });
 });
